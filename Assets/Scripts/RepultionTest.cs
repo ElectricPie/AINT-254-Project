a3ec30215d97d802ebd37a07699c3db3f2b7a4bf
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2017-10-11 16:28:07 +0100

    Attract/Repel Test
    A single object can successfully push and pull a target from itself however multiple objects cannot push the same target. Sphere has adjustable variables to control repulsion.

---
+++ Assets/Scripts/RepultionTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RepultionTest : MonoBehaviour {
+    [Range (0,2)]
+    public float strength;
+
+    public GameObject testObj;
+    public GameObject UI;
+
+    public bool repel;
+
+    private Vector3 m_direcToObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Get the direction of the other object
+        m_direcToObj = testObj.transform.position - transform.position;
+
+        UpdateUI();
+
+        if (repel == true)
+        {
+            Repel();
+        }
+        else
+        {
+            Attract();
+        }
+    }
+
+    void UpdateUI()
+    {
+        //Updates the UI to show the direction
+        UI.transform.GetChild(0).GetComponent<Text>().text = m_direcToObj.x.ToString();
+        UI.transform.GetChild(1).GetComponent<Text>().text = m_direcToObj.y.ToString();
+        UI.transform.GetChild(2).GetComponent<Text>().text = m_direcToObj.z.ToString();
+    }
+
+    void Repel()
+    {
+        Vector3 temp = new Vector3(m_direcToObj.x * strength, m_direcToObj.y * strength, m_direcToObj.z * strength); //Creates a vector in the direction of the well
+
+        testObj.GetComponent<Rigidbody>().velocity = temp; //Applys the vector to the object
+        //testObj.GetComponent<ObjectMovement>().UpdateVelocity(temp);
+    }
+
+    void Attract()
+    {
+        Vector3 temp = new Vector3(-m_direcToObj.x * strength, -m_direcToObj.y * strength, -m_direcToObj.z * strength);
+
+        testObj.GetComponent<Rigidbody>().velocity = temp;
+        //testObj.GetComponent<ObjectMovement>().UpdateVelocity(temp);
+    }
+}
